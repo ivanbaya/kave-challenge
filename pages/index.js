e5fd2538b62053b35ce24658e784dd263764c95e
Dodo <ivanbaya@gmail.com>
2022-01-18 11:31:59 +0100

    Categorias mas pequeñsa

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home({data}) {
   }else{
     numProductos = 8
   }
-  const randomNum = Math.floor(Math.random() * countProductes-numProductos) + 0
+  const randomNum = Math.floor(Math.random() * countProductes-numProductos)
 
   return (
     <>
